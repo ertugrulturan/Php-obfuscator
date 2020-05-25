08a591497edf0ad46f939eb931ab6669840903dd
Ertuğrul TURAN <60829297+obirninja@users.noreply.github.com>
2020-05-26 02:56:52 +0300

    :)
    :)

---
+++ obfuscator.php
@@ -0,0 +1,12 @@
+<?php
+// PHP obfuscator (Ultra +++)
+// https://obir.ninja - T13R - V4R1ABLE
+$phpkodlar = base64_decode("PD9waHAKZWNobyAiSEVMTE8gV09STEQiOwo/Pg=="); // YOUR PHP CODE BASE64_ENCODE END PASTE HERE AND OPEN PHP
+$t13r_sifreleme_methodu = strrev(base64_encode(gzdeflate(gzdeflate(gzcompress($phpkodlar)))));
+echo '
+<?php
+$obirninja = "'.$t13r_sifreleme_methodu.'";
+$t13r = "WlhaaGJDZ25QejRuTG1kNmRXNWpiMjF3Y21WemN5aG5lbWx1Wm14aGRHVW9aM3BwYm1ac1lYUmxLR0poYzJVMk5GOWtaV052WkdVb2MzUnljbVYyS0NSdlltbHlibWx1YW1FcEtTa3BLU2s3";
+eval(htmlspecialchars_decode(base64_decode(urldecode(base64_decode($t13r)))));
+?> '
+?>
